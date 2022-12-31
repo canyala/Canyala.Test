d52ac9cec2318bb09d3b777ce304741c82986cbe
Martin Fredriksson <martin.fredriksson@canyala.se>
2022-12-31 19:12:38 +0100

    Add Test.Tools files

---
+++ Canyala.Test.Tools/Context.cs
@@ -0,0 +1,32 @@
+﻿//
+// Copyright (c) 2012 Canyala Innovation AB
+//
+// All rights reserved.
+//
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Canyala.Test.Tools
+{
+    public static class Context
+    {
+        /// <summary>
+        /// Gets the full file path to a file name in the current application directory.
+        /// </summary>
+        /// <param name="fileName">the name of the file to get ("test.ttl" etc.)</param>
+        /// <returns>The full path to the file specified ("C:\Program Files\MyApp\test.ttl")</returns>
+        public static string TestFile(string fileName)
+        {
+            var applicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            return Path.Combine(Directory.GetParent(applicationBase).Parent.FullName, "TestFiles", fileName);
+        }
+
+        public static Temporary GetTemporary()
+            { return new Temporary(); }
+    }
+}
---
+++ Canyala.Test.Tools/Temporary.cs
@@ -0,0 +1,52 @@
+﻿//
+// Copyright (c) 2012 Canyala Innovation AB
+//
+// All rights reserved.
+//
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Canyala.Test.Tools
+{
+    public sealed class Temporary : IDisposable
+    {
+        public string FilePath { get; private set; }
+
+        internal Temporary() 
+            { FilePath = Path.GetTempFileName(); }
+
+        bool disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            try
+            {
+                if (!disposed)
+                {
+                    if (File.Exists(FilePath)) 
+                        File.Delete(FilePath);
+                }
+            }
+            finally
+            {
+                disposed = true;
+            }
+        }
+
+        ~Temporary()
+        {
+            Dispose(false);
+        }
+    }
+}
